25c24c5949552ffc0ae139d77717356d1935db2b
Ferhat Dursun <ferhatdursnu@gmail.com>
2022-06-06 10:09:40 +0200

    JS_FOR

---
+++ hausaufgabe.js
@@ -0,0 +1,70 @@
+//*! 1. Frage;
+
+const fiyatlar = [100, 250, 50, 89];
+
+//* p = fiyatlarin icerisinde ki elemanlari sirayla geziyor.
+//* i = p'nin sirayla aldigi elemanlarin index degeri.
+//* x = dizin.
+
+
+// fiyatlar.forEach((p,i,x) =>{
+
+//    x[i] = Math.round (p * 1.1);
+// }
+// )
+// console.log(fiyatlar);
+
+
+
+//*! 2. Frage;
+
+
+// for (let i of fiyatlar)
+
+//     if (i > 90)
+//         console.log(i)
+
+
+
+//*! 3. Frage;
+
+// a = []
+
+// for (let i of fiyatlar) {
+// console.log(a)
+
+//     if (i < 110)
+//         a =  Math.round(i * 1.1);
+// }
+
+
+//*! 4. Frage;
+
+const maaslar = [3000, 3200, 4000, 6000, 6500, 10000, 12000]
+
+a = []
+
+// for (let i of maaslar){
+// console.log(a)
+
+//     if (i < 4000)
+//         a = i * 1.5
+// }
+
+
+//*! 5. Frage;
+
+// for (let i of maaslar) {
+// console.log(a)
+
+//     if (i > 4000)
+//         a = i *1.25
+// }
+
+
+
+
+
+
+
+
